f3c6fdb983c8990ca088cb04c221114c8c802ec9
onny <onny.s@students.amikom.ac.id>
2020-10-31 16:35:28 +0700

    Menambahkan coding pada class bucketcontroller

--- FreshFruit/Controller/BucketController.cs
+++ FreshFruit/Controller/BucketController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FreshFruit.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,49 @@ namespace FreshFruit.Controller
 {
     class BucketController
     {
+        private Bucket bucket;
+        private BucketEventListener eventListener;
+
+        public BucketController(Bucket bucket, BucketEventListener eventListener)
+        {
+            this.bucket = bucket;
+            this.eventListener = eventListener;
+        }
+
+        public void addFruit(Fruit fruit)
+        {
+            if (bucketisOverload())
+            {
+                eventListener.onFailed("Oops, Keranjang Penuh");
+            }
+            else
+            {
+                this.bucket.insert(fruit);
+                eventListener.OnSucceed("Yey, berhasil ditambahkan");
+            }
+        }
+
+        public bool bucketIsoverload()
+        {
+            return bucket.countItems() >= bucket.getCapacity();
+        }
+
+        public void removeFruit(Fruit fruit)
+        {
+            for (int itemPosition = 0; itemPosition < bucket.countItems(); itemPosition++)
+            {
+                if (bucket.findAll().ElementAt(itemPosition).getName() == fruit.name)
+                {
+                    bucket.remove(itemPosition);
+                    eventListener.onSucceed("Yey, berhasil dihapus");
+                }
+            }
+          }
+
+        public list<Fruit> findAll()
+        {
+            return this.bucket.findAll();
+        }
+
+        }
     }
-}
