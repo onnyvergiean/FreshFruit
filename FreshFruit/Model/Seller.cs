93f0c0d1c44953c3ba59c4e30379325fcf8b8c3d
onny <onny.s@students.amikom.ac.id>
2020-10-31 16:25:42 +0700

    menambahakan coding pada class seller

--- FreshFruit/Model/Seller.cs
+++ FreshFruit/Model/Seller.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FreshFruit.Controller;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,23 @@ namespace FreshFruit.Model
 {
     class Seller
     {
+        private string name;
+        private BucketController bucket;
+
+        public Seller(string name, BucketController bucket)
+        {
+            this.name = name;
+            this.bucket = bucket;
+        }
+
+        public List<Fruit> findAll()
+        {
+            return this.bucket.findAll();
+        }
+
+        public void addFruit(Fruit fruit)
+        {
+            this.bucket.addFruit(fruit);
+        }
     }
 }
