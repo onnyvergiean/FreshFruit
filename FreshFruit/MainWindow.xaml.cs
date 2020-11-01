a7121595704c7269cca7c34f3de6490128af1dee
onny <onny.s@students.amikom.ac.id>
2020-11-01 14:20:11 +0700

    Menambahkan Coding pada MainWindow

--- FreshFruit/MainWindow.xaml.cs
+++ FreshFruit/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FreshFruit.Controller;
+using FreshFruit.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,31 +20,53 @@ namespace FreshFruit
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, BucketEventListener
     {
+        Seller toni;
         public MainWindow()
         {
             InitializeComponent();
+
+            Bucket keranjangBuah = new Bucket(2);
+            BucketController bucketController = new BucketController(keranjangBuah, this);
+
+            toni = new Seller("toni", bucketController);
+
+            ListBoxBucket.ItemsSource = keranjangBuah.findAll();
+        }
+
+        public void onFailed(string message)
+        {
+            MessageBox.Show(message, "Warning");
+        }
+
+        public void OnSucceed(string message)
+        {
+            ListBoxBucket.Items.Refresh();
         }
 
         private void Button1_Click(object sender, RoutedEventArgs e)
         {
-
+            Fruit anggur = new Fruit("Anggur");
+            toni.addFruit(anggur);
         }
 
         private void Button2_Click(object sender, RoutedEventArgs e)
         {
-
+            Fruit Apel = new Fruit("Apel");
+            toni.addFruit(Apel);
         }
 
         private void Button3_Click(object sender, RoutedEventArgs e)
         {
-
+            Fruit Pisang = new Fruit("Pisang");
+            toni.addFruit(Pisang);
         }
 
         private void Button4_Click(object sender, RoutedEventArgs e)
         {
-
+            Fruit jeruk = new Fruit("jeruk");
+            toni.addFruit(jeruk);
         }
     }
 }
